7a6b02c376fe6b9a1e6f9e45a5e36a85341d9851
Strzelczyn <Strzelczyn15@gmail.com>
2021-08-29 17:29:24 +0200

    ExtendedFloatingActionButton

--- app/src/main/java/com/example/jokeapp/ui/joke/JokeScreen.kt
+++ app/src/main/java/com/example/jokeapp/ui/joke/JokeScreen.kt
@@ -2,13 +2,14 @@ package com.example.jokeapp.ui.joke
 
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.jokeapp.data.model.Joke
@@ -17,16 +18,27 @@ import com.example.jokeapp.data.model.Joke
 fun JokeScreen(
     viewModel: JokeViewModel = hiltViewModel<JokeViewModel>()
 ) {
-    viewModel.add()
-    val jokes :List<Joke> by viewModel.data.observeAsState(emptyList())
-
-    LazyColumn(
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
-    ) {
-        items(
-            items = jokes,
-            itemContent = {
-                JokeListItem(joke = it)
-            })
-    }
+    val jokes: List<Joke> by viewModel.data.observeAsState(emptyList())
+    Scaffold(
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = { viewModel.add() },
+                elevation = FloatingActionButtonDefaults.elevation(8.dp),
+                modifier = Modifier.padding(end = 24.dp, bottom = 24.dp),
+                text = { Text(text = "New joke!!!") },
+            )
+        },
+        content = {
+            LazyColumn(
+                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+            ) {
+                items(
+                    items = jokes,
+                    itemContent = {
+                        JokeListItem(joke = it)
+                    })
+            }
+        }
+    )
 }
