d8c2753d0bd0257c7b3cbe80bf426c66cab5edd9
Strzelczyn <Strzelczyn15@gmail.com>
2021-09-04 10:11:23 +0200

    Add Test

---
+++ app/src/test/java/com/example/jokeapp/ui/joke/JokeViewModelTest.kt
@@ -0,0 +1,45 @@
+package com.example.jokeapp.ui.joke
+
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.jokeapp.api.JokeApi
+import com.example.jokeapp.data.model.Joke
+import com.example.jokeapp.data.repository.JokeRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.junit.Rule
+import org.junit.Test
+
+class JokeViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val sampleJoke = Joke(
+        1,
+        277,
+        null,
+        "\"99.9% of the people are dumb!\"",
+        "\"Fortunately I belong to the remaining 1%\"",
+        "Misc",
+        "twopart"
+    )
+
+    @Test
+    fun addTest() {
+        runBlocking {
+            //given
+            val api = mockk<JokeApi>()
+            val repository = mockk<JokeRepository>()
+            val viewModel = JokeViewModel(api, repository)
+            coEvery { api.getJoke() } returns sampleJoke
+            coEvery { repository.jokeAdd(any<Joke>()) } returns mockk()
+            //when
+            viewModel.add()
+            delay(3000)
+            //then
+        }
+    }
+}
